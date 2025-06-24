28504626d83d5782cd901af0103d0a482073c2a7
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-06-24 19:24:23 +0500

    refactor: improve type annotations for openCodes and closeCodes in applyStyles function

--- src/core/applyStyles.ts
+++ src/core/applyStyles.ts
@@ -1,14 +1,13 @@
 import { codes } from "./codes.ts";
 
 export function applyStyles(styles: string[], text: string): string {
-  let openCodes = "";
-  let closeCodes = "";
+  let openCodes: string = "";
+  let closeCodes: string = "";
 
   for (const style of styles) {
     const code = codes[style];
     if (code) {
       openCodes += code[0];
-      // prepend closing codes to keep order when nesting
       closeCodes = code[1] + closeCodes;
     }
   }
